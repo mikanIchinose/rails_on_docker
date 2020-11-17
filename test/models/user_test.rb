6b3c590a47d6a7c655f6fc8314631337380afbe8
mikan.ichinose <maruisansmai@gmail.com>
2020-11-17 17:54:46 +0900

    [update] emailを保存前に小文字化する

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -48,7 +48,15 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresse should be unique" do
     duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "email addresse should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
