8e4deacdfb30aa5e12659a6a9d2e975a00561545
mikan.ichinose <maruisansmai@gmail.com>
2020-11-22 00:54:03 +0900

    feat(flash): flashメッセージの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # 保存成功時
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user
     else
       render :new
     end
