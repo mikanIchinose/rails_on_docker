0313a991413da3b28436a50bd1f4b4f69b943938
mikan.ichinose <maruisansmai@gmail.com>
2020-11-21 19:38:11 +0900

    [add] フォームレイアウトとフォームアクションのテスト

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,12 +1,26 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "form layout" do
+    get signup_path
+    assert_select 'h1', 'New User'
+    assert_select 'input#user_name[type="text"]'
+    assert_select 'input#user_email[type="email"]'
+    assert_select 'input#user_password[type="password"]'
+    assert_select 'input#user_password_confirmation[type="password"]'
+  end
+
+  test "form action" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+  end
+
   # ユーザー情報が無効の状態で登録ボタンを押したときにユーザーが作成されない
   test "invalid signup information" do
     get signup_path
     # 無効なデータをpostしてもUser.countの数が変化しない
     assert_no_difference -> { User.count } do
-      post users_url, params: { user: { name: "",
+      post signup_path, params: { user: { name: "",
                                         email: "",
                                         password: "pass",
                                         password_confirmation: "word" } }
