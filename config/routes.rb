9cc0a47ef9fcc8e52d6c5250b096c79388d5a73f
mikan.ichinose <maruisansmai@gmail.com>
2020-11-23 23:57:14 +0900

    feat(session): ログイン/ログアウトへのルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help', as: 'helf'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :microposts
 
   resources :users
