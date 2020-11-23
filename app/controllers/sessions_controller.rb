d2dd1b3b0d556abdb0585ad442ce2968c927a0e3
mikan.ichinose <maruisansmai@gmail.com>
2020-11-24 05:32:38 +0900

    feat(login): ログイン時のヘルパーメソッドの定義
    log_in: ログインに成功したらユーザーIDをセッションに保持させる
    current_user: ログインに成功したらユーザー情報をインスタンス変数に格納する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
       # ユーザー情報のページにリダイレクトする
+      redirect_to user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
